47608265328d97b9fe3d6ae889431801627ff6ae
Aaron Martin <aaron.martin@shopgate.com>
2020-02-13 15:01:26 -0600

    CCP-1961: added widget

---
+++ frontend/components/RedeemOptions/index.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { css } from 'glamor';
+import { I18n } from '@shopgate/engage/components';
+import connect from './connector';
+import RedeemOptionItem from './components/RedeemOptionItem';
+
+const optionWrapper = css({
+  display: 'flex',
+  width: '100%',
+  flexFlow: 'row wrap',
+  alignContent: 'space-between',
+  paddingLeft: 6,
+  paddingTop: 6,
+}).toString();
+
+const headline = css({
+  fontSize: '22px',
+  margin: '12px 16px',
+});
+
+/**
+ * RedeemOptions component
+ * @param {Object[]} redeemOptions Swell redemption option
+ * @param {Object} settings Widget settings object
+ * @return {JSX}
+ */
+const RedeemOptions = ({ redeemOptions, settings }) => {
+  if (!redeemOptions.length) {
+    return null;
+  }
+
+  const { backgroundColor, textColor, title } = settings || {};
+  return (
+    <div>
+      <h2 className={headline}>
+        {title || <I18n.Text string="swell.rewards" />}
+      </h2>
+      <div className={optionWrapper}>
+        {
+          redeemOptions.map(redeemOption => (
+            <RedeemOptionItem
+              key={`option-id-${redeemOption.id}`}
+              redeemOption={redeemOption}
+              backgroundColor={backgroundColor}
+              textColor={textColor}
+            />
+          ))
+        }
+      </div>
+    </div>
+  );
+};
+
+RedeemOptions.propTypes = {
+  redeemOptions: PropTypes.arrayOf(PropTypes.shape()),
+  settings: PropTypes.shape(),
+};
+
+RedeemOptions.defaultProps = {
+  redeemOptions: [],
+  settings: {},
+};
+
+export default connect(RedeemOptions);
